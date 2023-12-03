8b764930e95c722c5c2e2cc9e027a52a7341db89
sooshiance <s.katafract@gmail.com>
2023-12-03 09:50:18 +0400

    fixing url bugs

--- backend/user/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-
-from .views import *
-
-
-router = routers.SimpleRouter()
-
-router.register("", AuthViewsets,  basename="auth")
-router.register("change-password", PasswordChangeView, basename="password-change")
--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path , include
+from rest_framework import routers
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import *
+
+
+router = routers.SimpleRouter()
+
+router.register("", AuthViewsets,  basename="auth")
+router.register("change-password", PasswordChangeView, basename="password-change")
 
 
 urlpatterns = [
     path("login/", CustomObtainTokenPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-token"),
     path("token/verify/", TokenVerifyView.as_view(), name="verify-token"),
-    # path("", include('router.urls')),
+    path("", include(router.urls)),
 ]
