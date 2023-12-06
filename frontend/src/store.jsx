fb53b0584a38d94a1599a10ca42f190344c746ea
sooshiance <s.katafract@gmail.com>
2023-12-06 18:22:22 +0400

    add reducer and its config to frontend

---
+++ frontend/src/store.jsx
@@ -0,0 +1,18 @@
+import { configureStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-tuhnk'
+import {composeWithDevTools} from 'redux-devtools-extension'
+
+
+const reducer = combineReducers({})
+
+
+const initialState = {}
+
+
+const middleware = [thunk]
+
+
+const store = configureStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+
+export default store
